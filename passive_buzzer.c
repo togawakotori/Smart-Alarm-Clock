730e8bd403c75f0d394837eefd185bb321d10b45
togawakotori <Das_Elliot@163.com>
2018-04-17 10:57:11 +0800

    Light Version

--- passive_buzzer.c
+++ passive_buzzer.c
@@ -3,8 +3,6 @@
 #include <stdio.h>
 #include <time.h>
 
-//------------------------
-
 
 #define BuzPin    0
 #define TouchPin  1
@@ -34,27 +32,11 @@
 #define  CH6  882
 #define  CH7  990
 
-//int song_1[] = {CM3,CM5,CM6,CM3,CM2,CM3,CM5,CM6,CH1,CM6,CM5,CM1,CM3,CM2,
-//				CM2,CM3,CM5,CM2,CM3,CM3,CL6,CL6,CL6,CM1,CM2,CM3,CM2,CL7,
-//				CL6,CM1,CL5};
-
-//int beat_1[] = {1,1,3,1,1,3,1,1,1,1,1,1,1,1,3,1,1,3,1,1,1,1,1,1,1,2,1,1,
-//				1,1,1,1,1,1,3};
-//
-
-//int song_2[] = {CM1,CM1,CM1,CL5,CM3,CM3,CM3,CM1,CM1,CM3,CM5,CM5,CM4,CM3,CM2,
-//				CM2,CM3,CM4,CM4,CM3,CM2,CM3,CM1,CM1,CM3,CM2,CL5,CL7,CM2,CM1
-//				};
-
-//int beat_2[] = {1,1,1,3,1,1,1,3,1,1,1,1,1,1,3,1,1,1,2,1,1,1,3,1,1,1,3,3,2,3};
-
-
 int tmp = 0;
 
 int song_1[] = {CM3,CM3,CM4,CM5,CM5,CM4,CM3,CM2};
 int beat_1[] = {1,1,1,1,1,1,1,1};
 
-void Print(int);
 int main(void)
 {
 	int i, j;
@@ -75,41 +57,26 @@ int main(void)
         time_t result;
       
  
-  //  while (1){Print(digitalRead(TouchPin));}
-        
- 
- 
-//	   Print(digitalRead(TouchPin));
-	   printf("music is being played...\n");
+   
+     printf("START ALARMING...\n");
  
      int record,flag=1;
      while (flag){
-                	printf("%d\n",digitalRead(TouchPin));
+       printf("%d\n",digitalRead(TouchPin));
 		
-if (digitalRead(TouchPin)==0){record=0;
-                              softToneWrite(BuzPin, song_1[i%(sizeof(song_1)/4)]); 
-			      delay(beat_1[i%(sizeof(song_1)/4)] * 300);}		
-			else {if (record++>6) flag=0; 
-                              softToneWrite(BuzPin, 0);  
-                              delay(50);	}
-
-           
-        i++;
-            } 
+       if (digitalRead(TouchPin)==0){
+          record=0;
+          softToneWrite(BuzPin, song_1[i%(sizeof(song_1)/4)]); 
+	  delay(beat_1[i%(sizeof(song_1)/4)] * 300);
+       }		
+       else {
+         if (record++>6) flag=0; 
+         softToneWrite(BuzPin, 0);  
+         delay(50);	
+       }
+  
+       i++;
+    } 
                 
-
-	return 0;
+  return 0;
 }
- 
-void Print(int x){
-	if (x != tmp){
-		if (x == 0)
-			printf("...ON\n");
-		if (x == 1)
-			printf("OFF..\n");
-		tmp = x;
-	}
-}
-
- 
-
